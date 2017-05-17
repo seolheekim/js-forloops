cb3d16f89924b314fa932fefc2cf0acb3d349f5f
Seolhee Kim <seolhee@upspringmedia.com>
2017-05-16 16:22:03 -1000

    7

--- exercises.js
+++ exercises.js
@@ -64,6 +64,11 @@ Declare a variable named `stringOfNumbers` and assign its value to an empty stri
 
 Write a for-loop that concatenates a Number value into that string on each iteration, starting at `10` and continuing up to and including `20`. Console.log your result. It should read "1011121314151617181920"*/
 
+var stringOfNumbers = [];
+for(var i = 10; i <21; i++){
+  stringOfNumbers += i;
+  console.log(stringOfNumbers);
+}
 
 /* 5) Even Stevens
 Declare a variable named `evenNumArr` and assign its value to an empty array.
@@ -72,6 +77,13 @@ Write a for-loop that will push even numbers to the `evenNumArr` array. We want 
 Console.log your results.
 */
 
+var evenNumArr = [];
+for(var i = 2; i < 50; i++){
+  if(i % 2 === 0){
+  evenNumArr.push(i);
+  }
+}
+console.log(evenNumArr);
 
 /* 6) Up the Odds
 Declare a variable named `oddSum` and assign it to the Number value 0.
@@ -80,6 +92,13 @@ Write a for-loop that will sum up odd numbers to the `oddSum` variable. We want 
 Console.log your results.
 */
 
+var oddSum = 0;
+for(var i = 0; i < 28; i++){
+  if(i % 2 === 1){
+    oddSum += i;
+  }
+}
+console.log(oddSum);
 
 /* 7) Oops There It is
 Declare a variable named `oopsArray` and assign its to the following array: `[ 'turn' , , 'down' , , 'for' , , 'what' ]`.
@@ -87,6 +106,15 @@ Note that every odd index value in `oopsArray` is currently `undefined`. Using a
 
 [ 'turn' , 'nope' , 'down' , 'nope' , 'for' , 'nope' , 'what' ]
 */
+
+var oopsArray = [ 'turn' , , 'down' , , 'for' , , 'what' ];
+
+for(var i = 0; i < oopsArray.length; i++){
+  if(i % 2 === 1){
+    oopsArray[i] = "nope";
+  }
+}
+console.log(oopsArray);
 
 
 /* 8) Is It There Oops
